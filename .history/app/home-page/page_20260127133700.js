86cb92f79ab8b130c3e816a1ce14008149b809d0
Micaela <micaela.valmores@gmail.com>
2026-02-01 15:24:30 -0800

    adding navbar and centering intro in homepage

---
+++ .history/app/home-page/page_20260127133700.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import styles from '../page.module.css'
+import FtProj from '../components/FtProj'
+import Navbar from '../components/Navbar'
+
+export default function HomePage() {
+  return (
+    <main className={styles.page}>
+      <nav className='Nav-bar'>
+        </nav>  
+      <Navbar />
+      <div className={styles['intro-sec']}>
+        <span className='intro-greet'>Hi, I am</span>
+        <h1>MICAELA</h1> 
+        <h1>VALMORES</h1>
+        <span className='intro-occu'>UX UI Designer</span>
+        <p>I turn complex problems into simple, accessible, and user-centred solutions that balance usability and aesthetics.</p>
+      </div>
+      <div className={styles['Proj-sec']}>
+        <h1>FEATURED PROJECTS</h1>
+        <FtProj reversed={false}/>
+        <FtProj reversed={true} />
+        <FtProj reversed={false}/> 
+      </div>
+    </main>
+  )
+}
---
+++ .history/app/home-page/page_20260127134302.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styles from '../page.module.css'
+import FtProj from '../components/FtProj'
+import Navbar from '../components/Navbar'
+
+export default function HomePage() {
+  return (
+    <main className={styles.page}>
+      <nav className='Nav-bar'>
+        </nav>  
+      <Navbar />
+      <div className={styles['intro-sec']}>
+        <span className={styles['intro-greet']}>Hi, I am</span>
+        <h1>MICAELA VALMORES</h1> 
+        <span className={styles['intro-occu']}>UX UI Designer</span>
+        <p>I turn complex problems into simple, accessible, and user-centred solutions that balance usability and aesthetics.</p>
+      </div>
+      <div className={styles['Proj-sec']}>
+        <h1>FEATURED PROJECTS</h1>
+        <FtProj reversed={false}/>
+        <FtProj reversed={true} />
+        <FtProj reversed={false}/> 
+      </div>
+    </main>
+  )
+}
