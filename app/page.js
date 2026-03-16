717e228e89b67f45df1b97ea8f2fe020fd67434c
Micaela <micaela.valmores@gmail.com>
2026-03-16 14:38:00 -0700

    applying responsiveness to the homepage

--- app/page.js
+++ app/page.js
@@ -1,11 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import HomePage from "./home-page/page";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <HomePage />
-    </div>
-  );
+  return <HomePage />;
 }
